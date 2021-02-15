f66bd0526e427adbe3baee64ff87213639179d3b
LE-Smith <djsschmidt@googlemail.com>
2021-02-15 15:47:00 +0100

    add String constants

--- src/App.js
+++ src/App.js
@@ -1,15 +1,116 @@
+import React, { useState } from 'react';
 import './App.css';
 
 import Formula from './components/Formula/Formula';
 import Result from './components/Result/Result';
 import Pads from './components/Pads/Pads';
 
+import * as String from './constants/Strings';
+
+
+
 function App() {
+  const [displayText, setDisplayText] = useState('0');
+  const [formulaString, setFormulaString] = useState(' ');
+  const [limitActive, setLimitActive] = useState(false);
+  
+  
+  const onMouseDownHandler = event => {
+    if (limitActive) return;
+    const char = event.target.innerText;
+    if (char === String.AC) {
+      initCalculator();
+      return;
+    }
+    if (char === String.EQUALS) {
+      console.log('CALCULATE NOW!!!!')
+      return;
+    }
+    if (displayText.length >= 18) {
+      showLimitMessage();
+      return;
+    }
+
+    // if (isCharIOForAddingToFormula(char)) {
+    //   setFormulaString(state => state + char);
+    // }
+
+    if (isCharIOForAddingToDisplay(char)) {
+      setDisplayText(state => state + char);
+    } else {
+      if (char === String.DECIMAL) {
+        return;
+      }
+      setDisplayText(char);
+    }
+  }
+
+  const initCalculator = () => {
+    setDisplayText('0');
+    setFormulaString(' ');
+  }
+
+  const showLimitMessage = () => {
+    const oldDisplayText = displayText;
+    setDisplayText('DIGIT LIMIT MET');
+    setLimitActive(true);
+    setTimeout(() => {
+      setDisplayText(oldDisplayText);
+      setLimitActive(false);
+    }, 1000);
+  }
+
+  const isCharIOForAddingToFormula = char => {
+    const lastCharIsNumberRegEx = /[0123456789]$/;
+    const lastCharIsDecimalRegEx = /\.$/;
+    const newSubtractIsOkRegEx = /([0123456789]+-{0,1}$)|(\s$)/;
+    const lastCharIsNumber = lastCharIsNumberRegEx.test(displayText);
+    const lastCharIsDecimal = lastCharIsDecimalRegEx.test(displayText);
+    const newCharIsNumber = lastCharIsNumberRegEx.test(char);
+    const newSubtractIsOk = newSubtractIsOkRegEx.test(formulaString);
+
+    if (char === String.SUBTRACT && newSubtractIsOk) {
+      return true;
+    }
+
+    if (char === String.DECIMAL && !displayText.includes(String.DECIMAL)) {
+      return true;
+    }
+
+    if ((lastCharIsNumber && newCharIsNumber) || (lastCharIsDecimal && newCharIsNumber)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  const isCharIOForAddingToDisplay = char => {
+    const lastCharIsNumberRegEx = /[0123456789]$/;
+    const lastCharIsDecimalRegEx = /\.$/;
+    const lastCharIsNumber = lastCharIsNumberRegEx.test(displayText);
+    const lastCharIsDecimal = lastCharIsDecimalRegEx.test(displayText);
+    const newCharIsNumber = lastCharIsNumberRegEx.test(char);
+
+    if (displayText === String.ZERO) {
+      return false;
+    }
+
+    if (char === String.DECIMAL && !(displayText.includes(String.DECIMAL))) {
+      return true;
+    }
+
+    if ((lastCharIsNumber && newCharIsNumber) || (lastCharIsDecimal && newCharIsNumber)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   return (
     <div className="App">
-      <Formula text="04434+554345 = 234234"/>
-      <Result text="6643424234"/>
-      <Pads />
+      <Formula text={formulaString}/>
+      <Result text={displayText}/>
+      <Pads onMouseDown={onMouseDownHandler}/>
     </div>
   );
 }
--- src/components/Pads/Pads.jsx
+++ src/components/Pads/Pads.jsx
@@ -1,30 +1,31 @@
 import React from 'react';
 import './Pads.css';
 
+import * as String from '../../constants/Strings';
+
 const Pads = props => {
 
     return (
-        <div id="pads">
-            <div id="clear">AC</div>
-            <div class="light-grey" id="divide">/</div>
-            <div class="light-grey" id="multiply">*</div>
-            <div class="dark-grey" id="seven">7</div>
-            <div class="dark-grey" id="eight">8</div>
-            <div class="dark-grey" id="nine">9</div>
-            <div class="light-grey" id="subtract">-</div>
-            <div class="dark-grey" id="four">4</div>
-            <div class="dark-grey" id="five">5</div>
-            <div class="dark-grey" id="six">6</div>
-            <div class="light-grey" id="add">+</div>
-            <div class="dark-grey" id="one">1</div>
-            <div class="dark-grey" id="two">2</div>
-            <div class="dark-grey" id="three">3</div>
-            <div class="dark-grey" id="zero">0</div>
-            <div class="dark-grey" id="decimal">.</div>
+        <div id="pads" onMouseDown={props.onMouseDown}>
+            <div id="clear">{String.AC}</div>
+            <div className="light-grey" id="divide">{String.DIVIDE}</div>
+            <div className="light-grey" id="multiply">{String.MULTIPLY}</div>
+            <div className="dark-grey" id="seven">{String.SEVEN}</div>
+            <div className="dark-grey" id="eight">{String.EIGHT}</div>
+            <div className="dark-grey" id="nine">{String.NINE}</div>
+            <div className="light-grey" id="subtract">{String.SUBTRACT}</div>
+            <div className="dark-grey" id="four">{String.FOUR}</div>
+            <div className="dark-grey" id="five">{String.FIVE}</div>
+            <div className="dark-grey" id="six">{String.SIX}</div>
+            <div className="light-grey" id="add">{String.ADD}</div>
+            <div className="dark-grey" id="one">{String.ONE}</div>
+            <div className="dark-grey" id="two">{String.TWO}</div>
+            <div className="dark-grey" id="three">{String.THREE}</div>
+            <div className="dark-grey" id="zero">{String.ZERO}</div>
+            <div className="dark-grey" id="decimal">{String.DECIMAL}</div>
             <div id="equals">=</div>
         </div>
     )
-
 }
 
 export default Pads;
